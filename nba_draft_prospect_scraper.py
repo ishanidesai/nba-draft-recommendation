3a904a828179046766c7bfd48a410a1e351afefe
Ishani Desai <ishanidesai@Ishanis-MacBook-Pro.local>
2020-08-13 12:55:20 -0700

    fixed bug in 'name' and added 'status'

--- nba_draft_prospect_scraper.py
+++ nba_draft_prospect_scraper.py
@@ -1,47 +1,76 @@
-#import requests
 import lxml.html as lh
 import pandas as pd
 from selenium import webdriver
 from urllib.request import Request, urlopen
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+import itertools as it
 import re
 import time
 
-url='https://www.nba.com/draft/2020/prospects#/'
+#url='https://www.nba.com/draft/2020/prospects#/'
 
-options = FirefoxOptions()
-options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
-driver.get(url)
-time.sleep(5) # wait to load
+# Names are scraped as Last,FirstF. Last...Change format to First Last
+def string_manipulate_name(full_str):
+    divided_str = full_str.split()
+    middle_str = ''
+    for str in divided_str:
+        if (sum(1 for c in str if c.isupper()) >= 2) and ('.' in str):
+            middle_str = str
+    delimiter = middle_str[-2:]
+    first_name = middle_str[:-2]
+    last_name = full_str.split(delimiter + ' ')[-1]
+    print(first_name + ' ' + last_name)
+    return first_name + ' ' + last_name
 
-# now print the response
-print(type(driver.page_source))
 
-doc = lh.fromstring(driver.page_source)
-driver.close()
-tr_elements = doc.xpath('//tr')
-tr_elements = tr_elements[1:]
+def scrape_nba_draft_prospects(url):
 
-# Remove unnecessary columns
-for el in tr_elements:
-    del el[1], el[5]
+    options = FirefoxOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
+    driver.get(url)
+    time.sleep(5) # wait to load
 
-# Names are scraped as Last,FirstF. Last
-# Change to Last, First
-name_list = []
-for el in tr_elements:
-    full_str = el[0].text_content()
-    name_list_case = re.findall('[A-Z][^A-Z]*', full_str)
-    #print(name_list_case)
-    name_list.append(name_list_case[0] + name_list_case[1])
+    # now print the response
+    print(type(driver.page_source))
 
-# Fill dictionary with data
-data_dict = { 'Name': name_list }
-for i in range(1, 6):
-    name = tr_elements[0][i].text_content().split()[0] # Manipulate string to get column names
-    info = [el[i].text_content().split()[1] for el in tr_elements]
-    data_dict[name] = info
+    doc = lh.fromstring(driver.page_source)
+    driver.close()
+    tr_elements = doc.xpath('//tr')
+    tr_elements = tr_elements[1:]
 
-df = pd.DataFrame.from_dict(data_dict)
-print(df.head())
+    # Remove unnecessary columns
+    for el in tr_elements:
+        print(el[0].text_content())
+        del el[1]#, el[5]
+
+    # Dictionary for prospect info
+    data_dict = {}
+
+    # Add 'Name' to dictionary
+    names = []
+    for el in tr_elements:
+        names.append(string_manipulate_name(el[0].text_content()))
+    data_dict['Name'] = names
+
+    # Add 'Status' to dictionary
+    if 'Status' in tr_elements[0][5].text_content():
+        col_info = [el[5].text_content().replace('Status', '') for el in tr_elements]
+        data_dict['Status'] = col_info
+
+    for i in it.chain(range(1, 5), range(6,7)):
+        name = tr_elements[0][i].text_content().split()[0] # Manipulate string to get column names
+        info = [el[i].text_content().replace((name + ' '), '') for el in tr_elements]
+        data_dict[name] = info
+
+    df = pd.DataFrame.from_dict(data_dict)
+    print(df.head())
+
+    return df
+
+
+def main():
+    scrape_nba_draft_prospects('https://www.nba.com/draft/2020/prospects#/')
+
+if __name__ == "__main__":
+    main()
